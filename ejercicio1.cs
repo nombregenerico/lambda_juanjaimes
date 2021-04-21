d191f6deeae275754c52106431cd526fb35bba52
nombregenerico <62416784+nombregenerico@users.noreply.github.com>
2021-04-21 01:49:25 -0500

    Add files via upload

---
+++ ejercicio1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ejercicios
+{
+    public sealed class DelegadoExpresionLambda
+    {
+        private static void Main()
+        {
+            // declaración de delegado genérico:
+            Func<int, int, int> suma;
+
+            // asignación de una expresión lambda:
+            suma = (int a, int b) => a + b;
+
+            Console.WriteLine("La suma de a más b es igual a {0}", suma(7, 4).ToString());
+        
+        }
+    }
+}
+
